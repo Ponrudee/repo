c2eb75975c5f4d67b38162dc8b8682894581e5e5
Ponrudee <67993201+Ponrudee@users.noreply.github.com>
2023-10-09 14:04:32 +0700

    Add files via upload

---
+++ avatar-extraction/jitar/src/org/langkit/tagger/languagemodel/LanguageModel.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2008 Daniel de Kok
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.langkit.tagger.languagemodel;
+
+import org.langkit.tagger.data.TriGram;
+
+/**
+ * Classes that implement the <i>LanguageModel</i> interface provide the
+ * <i>triGramProb</i> method that estimates the probability of a trigram,
+ * P(t3|t1,t2).
+ * 
+ */
+public interface LanguageModel {
+	/**
+	 * Estimate the probability of a trigram, P(t3|t1,t2), and return
+	 * the logprob.
+	 * @param triGram
+	 * @return
+	 */
+	double triGramProb(TriGram triGram);
+}
